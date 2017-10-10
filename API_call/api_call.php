4355a6530842a911b16c9a1e4ea1d942134e3e84
malshanucsc <randhajapvt@gmail.com>
2017-10-10 17:41:39 +0530

    url issue solved

--- API_call/api_call.php
+++ API_call/api_call.php
@@ -1,21 +1,27 @@
-
 <?php
-check();
+search();
 function search(){
 
-
-
-$service_url = 'https://api.github.com/search/repositories?q=login+language:php&sort=stars&order=desc';
+$service_url = "http://api.github.com/search/repositories?q=login&language=php&sort=stars&order=desc";
 $curl = curl_init();
-curl_setopt($curl,CURLOPT_URL,$service_url);
+//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+//curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl,CURLOPT_URL,$service_url );
 curl_setopt($curl, CURLOPT_HEADER, 0);
-//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($curl,CURLOPT_CONNECTTIMEOUT,1);
-$curl_response = curl_exec($curl);
-//$response = json_decode($curl_response);
-//die($response);
-//curl_close($curl);
-echo"$curl_response";
+	$curl_response = curl_exec($curl);
+
+//$json = file_get_contents($service_url);
+//$obj = json_decode($json,true);
+//echo $obj->access_token;
+
+
+
+$json = file_get_contents('http://api.github.com/search/repositories?q=login&language=php&sort=stars&order=desc');
+
+// Decode the JSON string into an object
+$obj = json_decode($json);
+var_dump($obj->results[0]->formatted_address);
+//echo "$obj";
 if ($curl_response === false) {
     $info = curl_getinfo($curl);
     curl_close($curl);
@@ -23,15 +29,14 @@ if ($curl_response === false) {
 }
 curl_close($curl);
 $decoded = json_decode($curl_response);
-if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-    die('error occured: ' . $decoded->response->errormessage);
-}
-echo 'response ok!';
-var_export($decoded->response);
+
+echo "$decoded";
+
+
 }
 
 
-$service_url = 'https://api.github.com/search/repositories?q=login+language:php&sort=stars&order=desc';
+$service_url = 'https://api.github.com/search/repositories?q=login&language=php&sort=stars&order=desc';
 
 $data=array(
 	"q"=>"login",
@@ -39,63 +44,27 @@ $data=array(
 	"sort"=>"stars",
 	"order"=>"desc",
 );
-//CallAPI("PUT",$service_url,$data);
-function CallAPI($method, $url, $data = false)
-{
-    $curl = curl_init();
 
-    switch ($method)
-    {
-        case "POST":
-            curl_setopt($curl, CURLOPT_POST, 1);
 
-            if ($data)
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-            break;
-        case "PUT":
-            curl_setopt($curl, CURLOPT_PUT, 1);
-            break;
-        default:
-            if ($data)
-                $url = sprintf("%s?%s", $url, http_build_query($data));
-    }
 
-    // Optional Authentication:
-    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
 
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-    $result = curl_exec($curl);
 
-    if ($result === false) {
-    $info = curl_getinfo($curl);
-    curl_close($curl);
-    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+
+
+
+
+
+
+
+
+function isJson($string) {
+ json_decode($string);
+ return (json_last_error() == JSON_ERROR_NONE);
 }
 
 
-echo "$result";
-    curl_close($curl);
 
-    return $result;
-}
-?>
 
-<?php
-function check(){
-// create a new cURL resource
-$ch = curl_init();
 
-// set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, "http://www.google.com/");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-// grab URL and pass it to the browser
-$curl_response =curl_exec($ch);
-echo "$curl_response";
-// close cURL resource, and free up system resources
-curl_close($ch);
-}
 ?>
